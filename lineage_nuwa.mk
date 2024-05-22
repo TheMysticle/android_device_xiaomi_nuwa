1edbe1e21ef40a468aa8e5e1945803a1cb99bbf2
IPSBHANGU <inderpreetbhangu2@gmail.com>
2024-05-22 12:46:10 +0530

    nuwa: Init LineageOS

---
+++ lineage_nuwa.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/nuwa
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit common Pixel Experience configurations
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier
+PRODUCT_DEVICE := nuwa
+PRODUCT_NAME := lineage_nuwa
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := 2210132G
+PRODUCT_MANUFACTURER := Xiaomi
+
+# GMS
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_NAME := nuwa_global
+PRODUCT_SYSTEM_DEVICE := nuwa
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="nuwa_global-user 14 UKQ1.230804.001 V816.0.1.0.UMBINXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Xiaomi/nuwa_global/nuwa:14/UKQ1.230804.001/V816.0.1.0.UMBINXM:user/release-keys
