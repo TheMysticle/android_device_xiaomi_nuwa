19d9a9b9396e0a0b8e14c0af137914c8bff1cbbf
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2024-11-28 00:44:23 +0100

    inline kernel building

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ $(call inherit-product, vendor/xiaomi/nuwa/nuwa-vendor.mk)
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Kernel Binary
-KERNEL_PREBUILT_DIR := device/xiaomi/nuwa-kernel
-
 # Overlays
 PRODUCT_PACKAGES += \
     NuwaFrameworks \
